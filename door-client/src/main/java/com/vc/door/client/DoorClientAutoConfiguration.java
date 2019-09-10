b6ac2c91600088a8bddc7f76aeaf21ca2477274e
jingling <jingling@dian.so>
2019-09-10 13:24:25 +0800

    修改自动配置

--- door-client/src/main/java/com/vc/door/client/DoorClientAutoConfiguration.java
+++ door-client/src/main/java/com/vc/door/client/DoorClientAutoConfiguration.java
@@ -1,12 +1,9 @@
 package com.vc.door.client;
 
-import com.vc.door.client.service.SsoLoginService;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan
 @Configuration
-@ConditionalOnBean(SsoLoginService.class)
 public class DoorClientAutoConfiguration {
 }
